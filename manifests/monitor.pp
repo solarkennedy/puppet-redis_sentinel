f529e7819fed83ad0259f73ab7067d8d9748779c
Kyle Anderson <kyle@xkyle.com>
2013-12-12 20:24:37 -0800

    Added init and monitor templates

---
+++ manifests/monitor.pp
@@ -0,0 +1,30 @@
+# == Define: redis_sentinel::monitor
+#
+# Sets up a particular piece of the Redis Sentinel config file to 
+# monitor a paritcular redis instance
+#
+# === Paramaters
+#
+# [*ensure*]
+#   Present or absent. Defaults to present.
+# 
+# ....
+# 
+define redis_sentinel::monitor (
+  $ensure                  => 'present',
+  $address                 => '127.0.0.1',
+  $port                    => '6379',
+  $quorum                  => '2',
+  $down_after_milliseconds => '6000',
+  $failover_timeout        => '9000',
+  $can_failover            => 'yes',
+  $parallel_syncs          => '5',
+) {
+
+  concat::fragment { "redis_sentinel_${name}":
+    ensure  => $ensure,
+    target  => $redis_sentinel::config_file,
+    content => template('redis_sentinel/monitor.erb'),
+  }
+
+}
