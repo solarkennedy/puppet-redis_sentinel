22404cf7ff064e55f5d6249b32243a548db16fd6
Kyle Anderson <kyle@xkyle.com>
2013-12-12 20:32:47 -0800

    Fix monitor

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -11,14 +11,14 @@
 # ....
 # 
 define redis_sentinel::monitor (
-  $ensure                  => 'present',
-  $address                 => '127.0.0.1',
-  $port                    => '6379',
-  $quorum                  => '2',
-  $down_after_milliseconds => '6000',
-  $failover_timeout        => '9000',
-  $can_failover            => 'yes',
-  $parallel_syncs          => '5',
+  $ensure                  = 'present',
+  $address                 = '127.0.0.1',
+  $port                    = '6379',
+  $quorum                  = '2',
+  $down_after_milliseconds = '6000',
+  $failover_timeout        = '9000',
+  $can_failover            = 'yes',
+  $parallel_syncs          = '5',
 ) {
 
   concat::fragment { "redis_sentinel_${name}":
