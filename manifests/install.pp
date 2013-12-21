756ac173a78e786695797dae8bfc43f92e2ed52c
Kyle Anderson <kwa@yelp.com>
2013-12-20 16:57:06 -0800

    Added upstart capabilities

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,12 +2,32 @@
 #
 class redis_sentinel::install {
 
-  file { '/etc/init.d/redis-sentinel':
-    ensure   => $redis_sentinel::ensure,
-    mode     => '0555',
-    owner    => 'root',
-    group    => 'root',
-    content => template('redis_sentinel/init.erb'),
-  }
+  #TODO: This isn't really a good way to figure out if an OS is upstart capable or not
+  case $::osfamily {
+    'RedHat': {
+      file { '/etc/init.d/redis-sentinel':
+        ensure   => $redis_sentinel::ensure,
+        mode     => '0555',
+        owner    => 'root',
+        group    => 'root',
+        content => template('redis_sentinel/init.erb'),
+      }
+    }
+    'Debian': {
+      file { '/etc/init/redis-sentinel':
+        ensure   => $redis_sentinel::ensure,
+        mode     => '0555',
+        owner    => 'root',
+        group    => 'root',
+        content => template('redis_sentinel/upstart.erb'),
+      }
+      file { '/etc/init.d/redis-sentinel':
+       ensure => 'link',
+       target => '/lib/init/upstart-job',
+       force  => true,
+      }
 
+    }
+    default: { fail('Unknown OS') }
+  } 
 }
